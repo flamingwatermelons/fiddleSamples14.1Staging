1b71f3174264a1411811ee79f87c92f286f024fc
unknown <OctopusStaging@S0106WEB3S.IGWEB.LOCAL>
2014-03-18 13:21:42 -0400

    SB build 14.1.20141.144

--- EN/HtmlSamples/splitter/nested-splitters/demo.js
+++ EN/HtmlSamples/splitter/nested-splitters/demo.js
@@ -14,8 +14,6 @@ $(function () {
                 width: "100%",
                 height: "500px",
                 crosshairVisibility: "visible",
-                verticalZoomable: true,
-                horizontalZoomable: true,
                 overviewPlusDetailPaneVisibility: "visible",
                 overviewPlusDetailPaneBackgroundImageUri: "http://staging.igniteui.local/14-1/images/samples/splitter/world.png",
                 panModifier: "control",
